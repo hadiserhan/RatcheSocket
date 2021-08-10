9917e96a8447b51c4d4368cef25acde46ab14d2a
hadi <hadi@softview-me.com>
2021-08-11 01:08:54 +0300

    db

--- database/Database_connection.php
+++ database/Database_connection.php
@@ -6,7 +6,7 @@ class Database_connection
 {
 	function connect()
 	{
-		$connect = new PDO("mysql:host=8.tcp.ngrok.io; dbname=chat", "root", "H@di123456");
+		$connect = new PDO("mysql:host=8.tcp.ngrok.io;port=17401; dbname=chat", "root", "H@di123456");
 
 		return $connect;
 	}
