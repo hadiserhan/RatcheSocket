36068c791efd49806f7d39d365968f862f10fd24
hadi <hadi@softview-me.com>
2021-08-12 00:04:12 +0300

    new

--- bin/server.php
+++ bin/server.php
@@ -18,18 +18,18 @@ use MyApp\Chat;
     // $server->route('/socket', new Chat(), $allow);
 
     // $server->run();
-    $server = new Ratchet\App('wwebsoketserver.herokuapp.com', 8834,'0.0.0.0');
-    $server->route('/chat', new Chat, array('*'));
-    $server->route('/echo', new Ratchet\Server\EchoServer, array('*'));
+    // $server = new Ratchet\App('wwebsoketserver.herokuapp.com', 8834,'0.0.0.0');
+    // $server->route('/chat', new Chat, array('*'));
+    // $server->route('/echo', new Ratchet\Server\EchoServer, array('*'));
   
-    // $server = IoServer::factory(
-    //     new HttpServer(
-    //         new WsServer(
-    //             new Chat()
-    //         )
-    //     ),
-    //     8080
-    // );
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new Chat()
+            )
+        ),
+        8834
+    );
 
     $server->run();
 
