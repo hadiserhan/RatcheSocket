615136a7a6deb061158b042867a5c35675e827d3
hadi <hadi@softview-me.com>
2021-08-11 22:25:06 +0300

    er

--- chatroom.php
+++ chatroom.php
@@ -205,7 +205,7 @@ $user_data = $user_object->get_user_all_data();
 	
 	$(document).ready(function(){
 
-		var conn = new WebSocket('wss://test1serversocket.herokuapp.com:6969');
+		var conn = new WebSocket('ws://test1serversocket.herokuapp.com');
 		conn.onopen = function(e) {
 		    console.log("Connection established!");
 		};
