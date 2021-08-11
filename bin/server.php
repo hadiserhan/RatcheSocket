bfd70f2ad841730c9d63e2e22ec62c85b20a2e61
hadi <hadi@softview-me.com>
2021-08-11 17:18:25 +0300

    domain

--- bin/server.php
+++ bin/server.php
@@ -6,29 +6,29 @@ use Ratchet\Server\IoServer;
 use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 use MyApp\Chat;
-use Ratchet\App;
+
     require dirname(__DIR__) . '/vendor/autoload.php';
 
-    $allow = ['localhost', 'wwebsoketserver.herokuapp.com'];
+    // $allow = ['localhost', 'wwebsoketserver.herokuapp.com'];
 
-    $server = new App('wwebsoketserver.herokuapp.com', 8080);
+    // $server = new App('wwebsoketserver.herokuapp.com', 8080);
 
-    //$server->route('/wamp', new Wamp(), $allow);
-    //$server->route('/echo', new EchoServer(), $allow);
-    $server->route('/socket', new Chat(), $allow);
-
-    $server->run();
-
-    // $server = IoServer::factory(
-    //     new HttpServer(
-    //         new WsServer(
-    //             new Chat()
-    //         )
-    //     ),
-    //     8082
-    // );
+    // //$server->route('/wamp', new Wamp(), $allow);
+    // //$server->route('/echo', new EchoServer(), $allow);
+    // $server->route('/socket', new Chat(), $allow);
 
     // $server->run();
+
+    $server = IoServer::factory(
+        new HttpServer(
+            new WsServer(
+                new Chat()
+            )
+        ),
+        8080
+    );
+
+    $server->run();
 
 
 ?>
